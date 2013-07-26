bc2bc8a509f6544aa26ba7c33700f6642def3a52
Cassandra <cjames21082@gmail.com>
2013-07-26 13:57:54 -0700

    add alembic

---
+++ alembic/versions/36067d5c5714_testing.py
@@ -0,0 +1,22 @@
+"""testing
+
+Revision ID: 36067d5c5714
+Revises: None
+Create Date: 2013-07-26 13:50:47.301498
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '36067d5c5714'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
